92bef2f5d3a771ddcc6a39f7b742e975cf665e0e
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-02-06 17:32:49 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,4 +1,14 @@
-
+function forLoop(array){
+  for (let i = 0; i < 25; i++){
+    of (i === 1){
+      array.push("I am 1 strange loop")
+    }
+    else{
+      array.push(`I am ${i} strange loops`)
+    }
+  }
+  return array
+}
 
 function makeArray() {
   const array = []
