aa00d8c3a7ff912d62081a8d5811a006e1f1acb6
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-02-06 17:37:54 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -11,42 +11,13 @@ function forLoop(array){
 }
 
 function whileLoop(num){
-  while (num >= 0){
+  while (num > 0){
     console.log(num)
     num --
   }
   console.log("done")
 }
-// function makeArray() {
-//   const array = []
-//   const t = 10
 
-//   for (let i = 0; i < t; i++) {
-//     array.push("I am a strange loop.")
-//   }
-
-//   return [array, t]
-// }
-
-// describe('loops', () => {
-//   jsdom({
-//     src: fs.readFileSync(path.resolve(__dirname, '..', 'loops.js'), 'utf-8')
-//   })
-
-//   describe('forLoop(array)', () => {
-//     it('adds `"I am ${i} strange loop${i === 0 ? \'\' : \'s\'}."` to an array 25 times', () => {
-//       const [array, t] = makeArray()
-//       const strangeArray = forLoop(array)
-//       const testArray = strangeArray.slice(array.length)
-
-//       let first = "I am 1 strange loop."
-//       let rest = "I am 24 strange loops."
-
-//       expect(strangeArray[11]).to.equal(first)
-//       expect(strangeArray[34]).to.equal(rest)
-//       expect(strangeArray.length).to.equal(t + 25)
-//     })
-//   })
 
 //   describe('whileLoop(n)', () => {
 //     it('counts down from n to 0', () => {
