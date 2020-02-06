23e381c4701a97b77fba25352686b8016e1c8f0a
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-02-06 17:32:54 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -10,60 +10,60 @@ function forLoop(array){
   return array
 }
 
-function makeArray() {
-  const array = []
-  const t = 10
+// function makeArray() {
+//   const array = []
+//   const t = 10
 
-  for (let i = 0; i < t; i++) {
-    array.push("I am a strange loop.")
-  }
+//   for (let i = 0; i < t; i++) {
+//     array.push("I am a strange loop.")
+//   }
 
-  return [array, t]
-}
+//   return [array, t]
+// }
 
-describe('loops', () => {
-  jsdom({
-    src: fs.readFileSync(path.resolve(__dirname, '..', 'loops.js'), 'utf-8')
-  })
+// describe('loops', () => {
+//   jsdom({
+//     src: fs.readFileSync(path.resolve(__dirname, '..', 'loops.js'), 'utf-8')
+//   })
 
-  describe('forLoop(array)', () => {
-    it('adds `"I am ${i} strange loop${i === 0 ? \'\' : \'s\'}."` to an array 25 times', () => {
-      const [array, t] = makeArray()
-      const strangeArray = forLoop(array)
-      const testArray = strangeArray.slice(array.length)
+//   describe('forLoop(array)', () => {
+//     it('adds `"I am ${i} strange loop${i === 0 ? \'\' : \'s\'}."` to an array 25 times', () => {
+//       const [array, t] = makeArray()
+//       const strangeArray = forLoop(array)
+//       const testArray = strangeArray.slice(array.length)
 
-      let first = "I am 1 strange loop."
-      let rest = "I am 24 strange loops."
+//       let first = "I am 1 strange loop."
+//       let rest = "I am 24 strange loops."
 
-      expect(strangeArray[11]).to.equal(first)
-      expect(strangeArray[34]).to.equal(rest)
-      expect(strangeArray.length).to.equal(t + 25)
-    })
-  })
+//       expect(strangeArray[11]).to.equal(first)
+//       expect(strangeArray[34]).to.equal(rest)
+//       expect(strangeArray.length).to.equal(t + 25)
+//     })
+//   })
 
-  describe('whileLoop(n)', () => {
-    it('counts down from n to 0', () => {
-      const spy = chai.spy.on(console, 'log')
-      const n = Math.floor(Math.random() * 100)
+//   describe('whileLoop(n)', () => {
+//     it('counts down from n to 0', () => {
+//       const spy = chai.spy.on(console, 'log')
+//       const n = Math.floor(Math.random() * 100)
 
-      expect(whileLoop(n)).to.equal('done')
-      expect(spy).to.have.been.called.exactly(n)
+//       expect(whileLoop(n)).to.equal('done')
+//       expect(spy).to.have.been.called.exactly(n)
 
-      console.log.reset()
-    })
-  })
+//       console.log.reset()
+//     })
+//   })
 
-  describe('doWhileLoop(num)', () => {
-    it ('console logs "I run once regardless." 1 time when passed an integer of 0 as a parameter.', () => {
-        const spy = chai.spy.on(console, 'log');
-        doWhileLoop(0);
-        expect(spy).to.have.been.called.exactly(1);
-    })
+//   describe('doWhileLoop(num)', () => {
+//     it ('console logs "I run once regardless." 1 time when passed an integer of 0 as a parameter.', () => {
+//         const spy = chai.spy.on(console, 'log');
+//         doWhileLoop(0);
+//         expect(spy).to.have.been.called.exactly(1);
+//     })
 
-    it ('console logs "I run once regardless." 10 times when passed an integer of 10 as a parameter.', () => {
-      const spy = chai.spy.on(console, 'log');
-      doWhileLoop(10);
-      expect(spy).to.have.been.called.exactly(10);
-    })
-  })
-})
+//     it ('console logs "I run once regardless." 10 times when passed an integer of 10 as a parameter.', () => {
+//       const spy = chai.spy.on(console, 'log');
+//       doWhileLoop(10);
+//       expect(spy).to.have.been.called.exactly(10);
+//     })
+//   })
+// })
